aabe5292abb60ba54959a00526385b7e996b0cb2
yaozongbin <78633585+yaozongbin@users.noreply.github.com>
2024-03-06 22:12:19 +0800

    Create 9.手写防抖.js

---
+++ JS/9.手写防抖.js
@@ -0,0 +1,26 @@
+//防抖
+//防抖函数的应用场景：
+● 按钮提交场景：防⽌多次提交按钮，只执⾏最后提交的⼀次 
+● 服务端验证场景：表单验证需要服务端配合，只执⾏⼀段连续的输⼊事件的最后⼀次，还有搜索联想词功能类似⽣存环境请⽤lodash.debounce 
+
+function debounce(fn, delay) {
+  let timer = null;
+  const _debounce = function (...args) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.call(this, ...args);
+    }, delay);
+  };
+  return _debounce;
+}
+
+function debounce(fn, delay) {
+  let timer = null;
+  const _debounce = function (...args) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.call(this, ...args);
+    }, delay);
+  };
+  return _debounce;
+}
