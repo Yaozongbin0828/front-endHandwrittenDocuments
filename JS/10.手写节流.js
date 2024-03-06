8e2e710612ff7311fe0db3597b19177e289bcc9b
yaozongbin <78633585+yaozongbin@users.noreply.github.com>
2024-03-06 22:17:25 +0800

    Create 10.手写节流.js

---
+++ JS/10.手写节流.js
@@ -0,0 +1,29 @@
+//节流
+节流函数的适⽤场景： 
+● 拖拽场景：固定时间内只执⾏⼀次，防⽌超⾼频次触发位置变动 
+● 缩放场景：监控浏览器resize 
+● 动画场景：避免短时间内多次触发动画引起性能问题
+
+function throttle(fn, delay) {
+  let last = 0;
+  const _throttle = function (...args) {
+    const now = new Date().getTime();
+    if (now - last >= delay) {
+      fn.call(this, ...args);
+      last = now;
+    }
+  };
+  return _throttle;
+}
+
+function throttle(fn, delay) {
+  let last = 0;
+  const _throttle = function (...args) {
+    const now = new Date().getTime();
+    if (now - last >= delay) {
+      fn.call(this, ...args);
+      last = now;
+    }
+  };
+  return _throttle;
+}
